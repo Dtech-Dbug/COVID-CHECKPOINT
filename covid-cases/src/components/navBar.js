7a7e68be9af8dc01a1c47488edeca0c32e160e83
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-04-24 23:46:28 +0530

    CRA installed

---
+++ covid-cases/src/components/navBar.js
@@ -0,0 +1,19 @@
+export  const NavBar = ()=> {
+
+
+
+    return (
+        <div className='navigation'>
+            <h1>Hello</h1>
+
+            <div className='nav-Lists'>
+                <ul>
+                    <li>About</li>
+                    <li>About</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        </div>
+    )
+
+}
